93432e3453098c32a10322468ebc2a65b750889b
zhangpeng <scbeta@qq.com>
2020-03-03 12:13:15 +0800

    准备发布1.0.1版本。

---
+++ Quick.Fields/Quick.Fields/AppSettings/Model.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Quick.Fields.AppSettings
+{
+    public class Model
+    {
+        public FieldForGet[] Fields { get; set; }
+
+        public string GetValue(string fieldId)
+        {
+            if (Fields == null || Fields.Length == 0)
+                return null;
+            foreach (var field in Fields)
+                if (field.Id == fieldId)
+                    return field.Value;
+            return null;
+        }
+
+        public static Model Load()
+        {
+            return Load("appsettings.json");
+        }
+
+        public static Model Load(string path)
+        {
+            var content = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<Model>(content);
+        }
+    }
+}
