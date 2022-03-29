a0c3a5765cadd7c346890b0f7008d698dbe7fad9
张鹏 <scbeta@qq.com>
2022-03-29 20:26:15 +0800

    FieldForGet增加InputTextArea_Rows属性。

---
+++ Quick.Fields/Quick.Fields/FieldForGet_InputTextArea.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quick.Fields
+{
+    public partial class FieldForGet
+    {
+        /// <summary>
+        /// 输入控件验证用正则表达式，当Type为Input时有效
+        /// </summary>
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public int? InputTextArea_Rows { get; set; }
+    }
+}
